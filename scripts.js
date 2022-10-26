5ab3c42f3f0f4e9e8799db0839f50fe730f3661a
RaayNoff <kraxtv471365@gmail.com>
2022-10-27 01:03:42 +0700

    Finished dz number 1

--- scripts.js
+++ scripts.js
@@ -4,13 +4,30 @@ const searchInput = document.querySelector(".js-input");
 
 loadBtn.addEventListener("click", function (evt) {
   const searchValue = searchInput.value.trim().toLowerCase();
-  fetch(`https://api.github.com/users/${searchValue}`).then(
-    (data) =>
-      (resultsContainer.innerHTML = `<div class="response-container">
+  console.log("SearchValue: ", searchValue);
+
+  fetch(`https://api.github.com/users/${searchValue}`)
+    .then((res) => {
+      if (res.status >= 200 && res.status < 300) {
+        return res;
+      } else {
+        throw new Error(res.status);
+      }
+    })
+    .then((res) => res.json())
+    .then(
+      (data) =>
+        (resultsContainer.innerHTML = `<div class="response-container">
                 <img src="${data.avatar_url}">
                 <p> Имя: <span>${data.name}</span><p>
                 <p> О себе: <span>${data.bio}</span><p>
                 <p> Кол-во репозиториев: <span>${data.public_repos}</span><p>
             </div>`)
-  );
+    )
+    .catch((error) => {
+      const alertErrMsg = "Не удалось найти пользователя!\nПричина: ";
+      if (Number(error.message) === 404 && searchValue.length === 0) {
+        alert(alertErrMsg + "Строка поиска пуста.");
+      } else alert(alertErrMsg + `Сервер ответил с кодом ${error.message}`);
+    });
 });
---
+++ users.js
@@ -0,0 +1,28 @@
+const printContainer = document.querySelector(".js-results");
+const callbackbtn = document.querySelector(".js-load");
+
+const usersRequestHandler = async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    console.log("Данные о пользователях: ", response.data);
+    const users = response.data;
+
+    users.forEach((data) => {
+      console.log(data);
+      printContainer.innerHTML += `<div class="response-container">
+		 <p> Имя: <span>${data.name}</span><p>
+		 <p> Никнейм: <span>${data.username}</span><p>
+		 <p> Город: <span>${data.address.city}</span><p>
+		 <p> Email: <span>${data.email}</span><p>
+		 <p> Сайт: <span>${data.website}</span><p>
+		 <p> Телефон: <span>${data.phone}</span><p>
+		 </div>`;
+    });
+  } catch (error) {
+    alert("Произошла ошибка при запросе данных о пользователях");
+  }
+};
+
+callbackbtn.addEventListener("click", usersRequestHandler);

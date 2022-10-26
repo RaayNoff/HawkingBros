36189d099e66a4c13f6b547d795e0d6eb6a69c18
RaayNoff <kraxtv471365@gmail.com>
2022-10-27 01:50:35 +0700

    Finished input

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-const input = document.getElementsByClassName();
+const input = document.querySelector(".wrapper__input");
+const errorField = document.querySelector(".wrapper__errmsg");
+
+const inputChangeHandler = (event) => {
+  try {
+    const value = event.target.value;
+    if (value.length === 0) {
+      throw new Error("Вы не ввели значение");
+    } else if (value.match(/\D+/gi)) {
+      throw new Error("Текстовое содержимое не является числом");
+    } else if (value < 5) {
+      throw new Error("Значение меньше 5");
+    } else if (value > 10) {
+      throw new Error("Значение больше 10");
+    } else {
+      throw new Error("");
+    }
+  } catch (error) {
+    errorField.innerHTML = error.message;
+  }
+};
+
+input.addEventListener("input", inputChangeHandler);

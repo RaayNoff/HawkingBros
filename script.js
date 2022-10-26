ccf326829f0fa4eb1508eff34b21210ea91fab0a
RaayNoff <kraxtv471365@gmail.com>
2022-10-27 02:06:34 +0700

    Finished lotery task

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-function lottery() {
+async function lottery() {
   console.log("Вы начали игру");
-  let promise = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      Math.random(0) > 0.5 ? resolve() : reject("Вы промахнулись");
-    }, 1000);
-  });
-  return promise;
+  try {
+    const pr = await new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        Math.random(0) > 0.5 ? resolve() : reject("Вы промахнулись");
+      }, 1000);
+    });
+    console.log("Вы выиграли");
+    console.log("Вам заплатили");
+  } catch (error) {
+    console.log("Вы проиграли");
+  } finally {
+    console.log("Игра закончена");
+  }
 }
 
-lottery()
-  .then(() => console.log("Вы выиграли"))
-  .then(() => console.log("Вам заплатили"))
-  .catch(() => console.log("Вы проиграли"))
-  .finally(() => console.log("Игра закончена"));
+lottery();
